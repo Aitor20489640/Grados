b5ea8c527f5686ed3bed7bf940bda6af8f6e5f76
aitgal <aitgal@ubuntu>
2022-02-17 17:20:07 +0100

    Añadido comentarios a la clase ConversorGrados

--- Grados/src/grados/ConversorGrados.java
+++ Grados/src/grados/ConversorGrados.java
@@ -11,34 +11,42 @@ package grados;
 public class ConversorGrados {
     private double celsius, fahrenheit;
     
+    //Constructo vacio de la clase
     public ConversorGrados() {
     }
-
+    
+    //Setter para el atributo celsius
     public void setCelsius(double celsius) {
         this.celsius = celsius;
     }
-
+    
+    //Setter para el atributo fahrenheit
     public void setFahrenheit(double fahrenheit) {
         this.fahrenheit = fahrenheit;
     }
-
+    
+    //Getter para el atributo celsius
     public double getCelsius() {
         return celsius;
     }
 
+    //Getter para el atributo fahrenheit
     public double getFahrenheit() {
         return fahrenheit;
     }
-
+    
+    // Consturctor con los dos atributos de la clase
     public ConversorGrados(double celsius, double fahrenheit) {
         this.celsius = celsius;
         this.fahrenheit = fahrenheit;
     }
     
+    //Metodo para convertir fahrenheit a celsius
     public double heittocelsius(double f){
         return ((f - 32) * 5 / 9);
     }
     
+    //Metodo para convertir celsius fahrenheit
     public double celsiustofahrenheit(double c){
         return ((c * 9 / 5) + 32);
     }
